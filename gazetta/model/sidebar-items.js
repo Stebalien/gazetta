9afada5e1e92ad041befbfd989450b72752df946
Steven Allen <steven@stebalien.com>
2015-06-20 09:51:27 -0400

    Update docs for 42c2764003e2b7cfa148cfc09c1ee1ed7995cbab
    Get rid of the author.
    
    This should be defined by the user.

--- gazetta/model/sidebar-items.js
+++ gazetta/model/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ContentSource",""]],"mod":[["index",""]],"struct":[["Entry","An entry in the website."],["Person",""],["Source",""],["StaticEntry","A static entry."]],"trait":[["Meta",""]],"type":[["Date",""]]});
+initSidebarItems({"enum":[["ContentSource",""]],"mod":[["index",""]],"struct":[["Entry","An entry in the website."],["Source",""],["StaticEntry","A static entry."]],"trait":[["Meta",""]],"type":[["Date",""]]});
