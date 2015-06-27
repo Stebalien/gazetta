c47e9b602e25e9862740d69cb61802ea9aada748
Steven Allen <steven@stebalien.com>
2015-06-27 12:39:19 -0400

    Update docs for e91552347a525933bfdabf4ef1dec914abcb5705
    Hash assets.
    
    This allows us to cache them.

---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gazetta'] = ["impl&lt;W, H&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='gazetta/util/struct.StreamHasher.html' title='gazetta::util::StreamHasher'>StreamHasher</a>&lt;W, H&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
