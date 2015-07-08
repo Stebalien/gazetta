e6f0668c80849674bcb7db47d0fea8b25e1ceb38
Steven Allen <steven@stebalien.com>
2015-07-08 12:51:00 -0400

    License

--- src/render.rs
+++ src/render.rs
@@ -1,3 +1,18 @@
+/* Copyright (2015) Stevem Allen
+ *
+ * This file is part of gazetta.
+ * 
+ * gazetta-bin is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License (version 3) as published by the Free Software Foundation.
+ * 
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with Foobar.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 use std::io::{self, BufWriter};
 use std::fs::{self, File};
 use std::path::Path;
--- src/yaml.rs
+++ src/yaml.rs
@@ -1,2 +1,17 @@
+/* Copyright (2015) Stevem Allen
+ *
+ * This file is part of gazetta.
+ * 
+ * gazetta-bin is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License (version 3) as published by the Free Software Foundation.
+ * 
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with Foobar.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 pub use yaml_rust::{Yaml, ScanError};
 pub use yaml_rust::yaml::{Hash, Array};
