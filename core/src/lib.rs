183fd036bc42cecf82d6bd2a0ff2bc3e558a59b8
Steven Allen <steven@stebalien.com>
2015-09-11 18:59:31 -0400

    Update license

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,16 +1,17 @@
-/* Copyright (2015) Stevem Allen
+/*  Copyright (C) 2015 Steven Allen
  *
- * This file is part of gazetta.
- * 
- * gazetta is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Affero General Public License (version 3) as published by the Free Software Foundation.
- * 
- * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License along with Foobar.  If
- * not, see <http://www.gnu.org/licenses/>.
+ *  This file is part of gazetta.
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation version 3 of the
+ *  License.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program.  If
+ *  not, see <http://www.gnu.org/licenses/>.
  */
 
 //! TODO: Document.
