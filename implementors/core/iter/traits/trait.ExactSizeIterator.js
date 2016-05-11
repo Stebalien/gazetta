1a52a2c0e1156bbcc2f54f50f83b3d5c69b6f00f
Steven Allen <steven@stebalien.com>
2016-05-11 15:21:27 -0400

    Update docs for 2689a72af36c093b3c8f786e836fb634679474d3
    fix warning on nightly

---
+++ implementors/core/iter/traits/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['str_stack'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.ExactSizeIterator.html' title='core::iter::traits::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='str_stack/struct.Iter.html' title='str_stack::Iter'>Iter</a>&lt;'a&gt;",];implementors['lazy_static'] = [];implementors['libc'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['str_stack'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='str_stack/struct.StrStack.html' title='str_stack::StrStack'>StrStack</a>",];implementors['lazy_static'] = [];implementors['vec_map'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for <a class='struct' href='vec_map/struct.VecMap.html' title='vec_map::VecMap'>VecMap</a>&lt;T&gt;","impl&lt;'a, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='vec_map/struct.VecMap.html' title='vec_map::VecMap'>VecMap</a>&lt;T&gt;","impl&lt;'a, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a mut <a class='struct' href='vec_map/struct.VecMap.html' title='vec_map::VecMap'>VecMap</a>&lt;T&gt;",];implementors['libc'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
