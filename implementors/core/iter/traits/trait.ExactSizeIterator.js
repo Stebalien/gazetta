624cf82bbf9bf2f0ef892626e53ab05c347bfeaa
Steven Allen <steven@stebalien.com>
2016-09-13 18:07:38 -0400

    Update docs for c2630952c863aed5bfc242aba0e7ec9ad25f1469
    Fix footnote links

--- implementors/core/iter/traits/trait.ExactSizeIterator.js
+++ implementors/core/iter/traits/trait.ExactSizeIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["str_stack"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.ExactSizeIterator.html' title='core::iter::traits::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='str_stack/struct.Iter.html' title='str_stack::Iter'>Iter</a>&lt;'a&gt;",];
+implementors["lazy_static"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/trait.Not.js
+++ implementors/core/ops/trait.Not.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["pulldown_cmark"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='pulldown_cmark/struct.Options.html' title='pulldown_cmark::Options'>Options</a>",];
+implementors["lazy_static"] = [];implementors["pulldown_cmark"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='pulldown_cmark/struct.Options.html' title='pulldown_cmark::Options'>Options</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
