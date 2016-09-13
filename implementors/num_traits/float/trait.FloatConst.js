84d9e850890fed88bab2650722fbcfcd765b15a3
Steven Allen <steven@stebalien.com>
2016-09-13 18:11:55 -0400

    Update docs for 2068708c216cdf75904b99fc94dc9ac51d2d3ea3
    Make sure we don't reload when navigating to footnotes

--- implementors/num_traits/float/trait.FloatConst.js
+++ implementors/num_traits/float/trait.FloatConst.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["num_traits"] = [];implementors["num"] = [];
+implementors["num_traits"] = [];implementors["num"] = [];implementors["num"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
