be64c4a94758ef10de8ea19a389168e4ed3432b6
Steven Allen <steven@stebalien.com>
2016-10-01 18:23:07 -0700

    Update docs for 3256651e22a4576faa3355b1a09fd3674ffef2e9
    Make clippy happier.

--- implementors/core/ops/trait.Not.js
+++ implementors/core/ops/trait.Not.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["pulldown_cmark"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='pulldown_cmark/struct.Options.html' title='pulldown_cmark::Options'>Options</a>",];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["pulldown_cmark"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='pulldown_cmark/struct.Options.html' title='pulldown_cmark::Options'>Options</a>",];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
