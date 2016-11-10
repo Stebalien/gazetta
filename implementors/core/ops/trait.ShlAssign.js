d8eb764b50a896c59e037f5fbe1ff1145445c20e
Steven Allen <steven@stebalien.com>
2016-11-10 12:44:50 -0800

    Update docs for 1a84cd1949d114bd85f65846985d1e7dd9763007
    Fix deprecation

--- implementors/core/ops/trait.ShlAssign.js
+++ implementors/core/ops/trait.ShlAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
