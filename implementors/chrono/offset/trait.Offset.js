c86b3e9d20db15b47649c58bed176d1982205bc4
Steven Allen <steven@stebalien.com>
2016-11-10 12:40:01 -0800

    Update docs for 6ca23bacb93be737dd11c77294dd6509a5b5340c
    format and question mark

--- implementors/chrono/offset/trait.Offset.js
+++ implementors/chrono/offset/trait.Offset.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];
+implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/rand/reseeding/trait.Reseeder.js
+++ implementors/rand/reseeding/trait.Reseeder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rand"] = [];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];implementors["gazetta_model_ext"] = [];implementors["gazetta_core"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];implementors["gazetta_model_ext"] = [];
+implementors["rand"] = [];implementors["num"] = [];implementors["chrono"] = [];implementors["gazetta_core"] = [];implementors["gazetta_core"] = [];implementors["gazetta_model_ext"] = [];implementors["gazetta_model_ext"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
