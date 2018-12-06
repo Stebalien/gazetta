77499ed49a58be521e3f4d4ef215ded24b903f6e
Steven Allen <steven@stebalien.com>
2018-12-06 13:04:49 -0800

    update pulldown-cmark

--- render-ext/src/markdown.rs
+++ render-ext/src/markdown.rs
@@ -15,7 +15,7 @@
 //
 
 use horrorshow::prelude::*;
-use pulldown_cmark::{Event, Parser, OPTION_ENABLE_FOOTNOTES, OPTION_ENABLE_TABLES};
+use pulldown_cmark::{Event, Parser, Options};
 use std::borrow::Cow;
 use std::collections::HashMap;
 
@@ -57,7 +57,7 @@ impl<'a> Render for Markdown<'a> {
     fn render(&self, tmpl: &mut TemplateBuffer) {
         tmpl << RenderMarkdown {
             footnotes: HashMap::new(),
-            iter: Parser::new_ext(self.data, OPTION_ENABLE_TABLES | OPTION_ENABLE_FOOTNOTES),
+            iter: Parser::new_ext(self.data, Options::ENABLE_TABLES | Options::ENABLE_FOOTNOTES),
             base: self.base,
         }
     }
